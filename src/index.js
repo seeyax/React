292babc864d019374358d9ad2500a1d913dafd46
seeya <seeya@sztus.com>
2024-05-22 17:47:29 +0800

    proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//  多ES6内置API做兼容处理
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/stable'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
