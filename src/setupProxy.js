44fac13df90d00647abaef5f07c80ea72e69ba01
seeya <seeya@sztus.com>
2024-05-23 14:27:42 +0800

    delete proxy

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-module.exports = function(app) {
-  app.use(
-    createProxyMiddleware("/jian",{
-      target: 'https://www.jianshu.com/asimov',
-      changeOrigin: true
-    })
-  )
-}
