184ad78c214a0682ca0716b4b72fa7baae0a022a
seeya <seeya@sztus.com>
2024-05-30 17:12:47 +0800

    函数组件

--- src/index.jsx
+++ src/index.jsx
@@ -4,42 +4,14 @@ import ReactDOM from 'react-dom/client';
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/ie9'
 import 'react-app-polyfill/stable'
-import { createElement } from './jsxHandle';
+import DemoOne from './views/DemoOne';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let styObj = {
-  color:'red',
-  fontSize: '16px'
-}
-
-let x = 10;
-let y = 20;
-
 root.render(
   <div>
-    <h2 className="title" style={styObj}>珠峰培训</h2>
-    <div className="box">
-      <span>{x}</span>
-      <span>{y}</span>
-    </div>
+    珠峰培训
+    <DemoOne title="dhsj" x={10} data={[10,20]} className="box" style={{fontSize:'16px'}}></DemoOne>
   </div>
-)
-console.log(
-  createElement(
-    React.Fragment,
-    null,
-    createElement(
-      'h2',
-      {className: 'title', style: styObj},
-      'u73E0...'
-    ),
-    createElement(
-      'div',
-      { className: 'box'},
-      createElement('span',null,x),
-      createElement('span',null,y)
-    )
-  )
 )
---
+++ src/views/DemoOne.jsx
@@ -0,0 +1,7 @@
+const DemoOne = function (props) {
+  console.log(props)
+  return <div className="demo-box">
+    我是Demo-One
+  </div>
+}
+export default DemoOne
