f796e4e086cfbe4272d73a09997cef2d4bd1b677
seeya <seeya@sztus.com>
2024-06-03 17:14:54 +0800

    封装组件

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/ie9'
 import 'react-app-polyfill/stable'
-import DemoOne from './views/DemoOne';
+import DemoOne from './views/components/DemoOne';
+import Dialog from './views/components/Dialog';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,10 +16,14 @@ root.render(
       <span slot="footer">我是页脚</span>
       <span slot="header">我是页眉</span>
     </DemoOne>
-    {/* <DemoOne title="title-哈哈哈哈哈哈" className='box' style={{fontSize:'16px', color:'red'}} x={59}>
-      <span>我有children</span>
-    </DemoOne>
-    <DemoOne title="title-demo3" className="jkc" x={56} style={{fontSize:'16px', color:'red'}}/> */}
+    <br />
+    <br />
+    <br />
+    <Dialog title="友情提示" content="大家出门做好个人防护"/>
+    <Dialog content="我们一定要好好学习React!!">
+      <button slot="sure">确定</button>
+      <button slot="cancel">取消</button>
+    </Dialog>
   </div>
 )
 
---
+++ src/views/components/Dialog.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+const Dialog = function (props) {
+  let { title,content,children} = props
+  children = React.Children.toArray(children)
+  let sureSlot = [], cancelSlot = []
+  children.forEach(child => {
+    // 传递进来的插槽信息，都是编译为virtualDOM后传递进来的[而不是传递的标签]
+    console.log(child)
+    let { slot } = child.props
+    if(slot === 'sure') {
+      sureSlot.push(child)
+    } else if (slot === 'cancel') {
+      cancelSlot.push(child)
+    }
+  })
+  return <div className="dialog-box">
+    <div className="dialog-box">
+      <div className="header">
+        <div className="title">{title}</div>
+        <div className="content">{content}</div>
+      </div>
+      <div className="footer">
+        {sureSlot}
+        {cancelSlot}
+      </div>
+    </div>
+
+  </div>
+}
+/**属性规则校验 */
+Dialog.defaultProps = {
+  title: '温馨提示'
+}
+Dialog.propTypes = {
+  title:PropTypes.string,
+  content: PropTypes.string.isRequired
+}
+
+export default Dialog
