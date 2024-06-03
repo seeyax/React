70d131fef699b6c11cb89db0347368faa81a66ef
seeya <seeya@sztus.com>
2024-06-03 16:56:48 +0800

    slot

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <div>
-    珠峰培训
-    <DemoOne title="react" x={10} className='box' style={{fontSize:'22px'}}></DemoOne>
-    <DemoOne title="哈哈哈哈哈哈" className='box' style={{fontSize:'22px'}}></DemoOne>
+    <DemoOne title="title - react" x={10} className='box' style={{fontSize:'22px', color:'red'}}>
+      <span slot="footer">我是页脚</span>
+      <span slot="header">我是页眉</span>
+    </DemoOne>
+    {/* <DemoOne title="title-哈哈哈哈哈哈" className='box' style={{fontSize:'16px', color:'red'}} x={59}>
+      <span>我有children</span>
+    </DemoOne>
+    <DemoOne title="title-demo3" className="jkc" x={56} style={{fontSize:'16px', color:'red'}}/> */}
   </div>
 )
 
--- src/views/DemoOne.jsx
+++ src/views/DemoOne.jsx
@@ -1,10 +1,41 @@
 import PropTypes from 'prop-types'
+import React from 'react'
+console.log(React)
 const DemoOne = function (props) {
   // console.log(Object.isFrozen(props)) ==>true
-  let { className, style, title, x } = props
-  console.log(x)
-  return <div className={`demo-box ${className}`} style={style}>
-    <h2 className="title">{title}</h2>
+  let { className, style, title, x, children } = props
+  // 要对children的类型做处理
+  // if (children === undefined) {
+  //   children = []
+  // } else if (!Array.isArray(children)) {
+  //   children = [children]
+  // }
+  /** 可以基于React.Children对象中提供的方法，对props.children做处理
+   * count/forEach/map/only/toArray
+   * 优势：在这些方法的内部，已经对children的各种形式做了处理
+  */
+  
+  children = React.Children.toArray(children)
+  let headerSlot = [], footSlot = [], defaultSlot = []
+  children.forEach(child => {
+    // 传递进来的插槽信息，都是编译为virtualDOM后传递进来的[而不是传递的标签]
+    console.log(child)
+    let { slot } = child.props
+    if(slot === 'header') {
+      headerSlot.push(child)
+    } else if (slot === 'footer') {
+      footSlot.push(child)
+    } else {
+      defaultSlot.push(child)
+    }
+  })
+  return <div className={`demo-box ${className}`}>
+    <h2 style={style}>{title}</h2>
+    {headerSlot}
+    <br />
+    <span style={style}>x的值: {x}</span>
+    <br />
+    {footSlot}
   </div>
 }
 // 通过函数当作对象，设置静态的私有属性方法[把函数当作对象]，来给其设置属性的校验规则
@@ -15,7 +46,7 @@ DemoOne.defaultProps = {
 DemoOne.propTypes = {
   title: PropTypes.string.isRequired,
   x:PropTypes.number,
-  y: PropTypes.oneOffType([
+  y: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.string
   ])
