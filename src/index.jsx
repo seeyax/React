ffc818938bcf597ad3768b696d5fb6cc77bc1112
Seeya Xia <seeya@sztus.com>
2024-06-18 16:58:58 +0800

    change name2

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import 'react-app-polyfill/stable'
 // import Dialog from './views/components/Dialog';
 // import Vote from './views/Vote'
 // import NewVote from './views/NewVotes'
-import Demo from './views/DemoSetState'
+import Demo from './views/SetState'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
