5cabd7d0a7b14da225ad2724dd7eeba80b458498
Seeya Xia <seeya@sztus.com>
2024-06-26 16:44:37 +0800

    react合成事件原理1

--- src/views/Reactshijianhecheng.jsx
+++ src/views/Reactshijianhecheng.jsx
@@ -4,20 +4,21 @@ import React from 'react'
 class Demo extends React.Component {
   render() {
     // onClickCapture: 捕获阶段触发
+    // 1. 视图渲染中，遇到合成事件绑定，并没有给元素做事件绑定，而是给元素设置对应的属性'合成事件属性'
     return <div className="outer"
       onClick={()=>{
-        console.log('outer 冒泡');
+        console.log('outer 冒泡 [合成]');
       }}
       onClickCapture={() => {
-        console.log('outer 捕获');
+        console.log('outer 捕获 [合成]');
       }}
     >
       <div className="inner"
       onClick={()=>{
-        console.log('inner 冒泡');
+        console.log('inner 冒泡 [合成]');
       }}
       onClickCapture={() => {
-        console.log('inner 捕获');
+        console.log('inner 捕获 [合成]');
       }}
       ></div>
     </div>
@@ -30,9 +31,19 @@ class Demo extends React.Component {
     document.body.addEventListener('click', () => {
       console.log('body 冒泡');
     }, false)
-
+    // 2. 给#root做事件绑定[捕获/冒泡],root上绑定的方法执行，把所有规划的路径中，有合成事件属性的都执行即可
     let root = document.querySelector('#root')
-    root.addEventListener('click', () => {
+    root.addEventListener('click', (ev) => {
+      // console.log(ev);
+      // console.log(ev.path)
+      // 在这里把绑定的合成事件方法执行
+        // + 如果不经过处理，方法中的this是undefined[如果绑定的方法是箭头函数，则在函数上找上下文中的this]
+        // + 在执行这些方法之前，把原生的事件对象ev做特殊处理，返回合成事件对象，传递给函数
+      // let path = ev.path //path: [事件源-> ... -> window] 所有祖先元素
+      // [...path].reverse().forEach(ele => {
+      //   let handle = ele.onClickCapture;
+      //   if (handle) handle()
+      // })
       console.log('root 捕获');
     }, true)
     root.addEventListener('click', () => {
@@ -40,6 +51,25 @@ class Demo extends React.Component {
     }, false)
     // body 捕获 -> outer 捕获 -> inner 捕获 -> root 捕获 -> inner 冒泡 -> outer 冒泡 -> root 冒泡 -> body 冒泡
     // root是乱的
+
+    let outer = document.querySelector('.outer')
+    outer.addEventListener('click', () =>{
+      console.log('outer 捕获 [原生]');
+    }, true)
+    outer.addEventListener('click', () =>{
+      console.log('outer 冒泡 [原生]');
+    }, false)
+
+    let inner = document.querySelector('.inner')
+    inner.addEventListener('click', () =>{
+      console.log('inner 捕获 [原生]');
+    }, true)
+    inner.addEventListener('click', () =>{
+      console.log('inner 冒泡 [原生]');
+    }, false)
+    // 顺序是乱的 -->原因看：assets中React合成事件views中的输出原因
+    // body 捕获 -> outer 捕获 [合成] -> inner 捕获 [合成] -> root 捕获 -> outer 捕获 [原生] ->inner 捕获 [原生] 
+    // -> inner 冒泡 [原生] -> outer 冒泡 [原生] -> inner 冒泡 [合成] -> outer 冒泡 [合成] -> root 冒泡 ->body 冒泡
   }
 }
 
@@ -57,7 +87,17 @@ export default Demo
 
   然后对#root这个容器做了事件绑定[捕获和冒泡都做了]
     原因：因为组件中所渲染的内容，最后都会插入到#root这个容器中，这样点击页面中任何一个元素，最后都会把#root的点击行为触发
+    而在给#root绑定的方法中，把之前给元素设置的onXxx/onXxxCapture属性，在相应的阶段执行
 
  *  
  * 
  */
+
+  /**
+   * 合成事件原理
+   *  其实并没有给元素本身做事件绑定，而是给元素设置onXxx/onXxxCapture这样的合成事件属性！！
+   *  当事件行为触发，根据原生事件传播的机制，都会传播到#root容器上，React内部给#root容器做了事件绑定[捕获&冒泡]
+   *  当React内部绑定的方法执行的时候，会根据ev.path中分析的路径，依次把对应阶段的onXxx/onXxxCapture等事件合成属性触发执行
+   * 
+   *  总结：合成事件是利用事件委托（事件传播机制）完成的
+   */
