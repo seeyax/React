a569928329828a4fab54a326cc1ae765c081fab8
Seeya Xia <seeya@sztus.com>
2024-07-18 14:01:27 +0800

    函数组件的父子组件通信

---
+++ src/components/父子组件通信的核心思想（类组件）/VoteMain.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../index.less'
+import PropTypes from 'prop-types'
+
+class VoteMain extends React.Component {
+  // 属性规则校验
+   
+  static defaultProps = {
+    supNum: 0,
+    oppNum: 0
+  }
+  static propTypes = {
+    supNum: PropTypes.number,
+    oppNum: PropTypes.number
+  }
+  render () {
+    let { supNum, oppNum } = this.props
+    let ratio = '--'
+    let total = supNum + oppNum
+    if(total>0) {
+      ratio = (supNum/total*100).toFixed(2) + '%'
+    }
+    return <div className="main">
+    <p>支持人数：{supNum}人</p>
+    <p>反对人数：{oppNum}人</p>
+    <p>支持比率： {ratio}</p>
+  </div>
+  }
+}
+
+export default VoteMain
