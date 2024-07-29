1b249be6570323a207c0e39c22496f745c491465
Seeya Xia <seeya@sztus.com>
2024-07-29 18:05:29 +0800

    12

--- src/components/redux的基础操作和思想/VoteMain.jsx
+++ src/components/redux的基础操作和思想/VoteMain.jsx
@@ -1,33 +1,33 @@
-// import React from 'react'
-// import '../index.less'
-// import ThemeContext from '../ThemeContext'
+import React from 'react'
+import '../index.less'
+import ThemeContext from '../ThemeContext'
 
 
-// class VoteMain extends React.Component {
+class VoteMain extends React.Component {
 
-//   static contextType = ThemeContext
-//   // this.props
-//   render () {
-//     const { store } = this.context
-//     // 获取公共状态信息做绑定
-//     let {supNum, oppNum} = store.getState()
-//     return <div className="main">
-//       <p>支持人数：{supNum}人</p>
-//       <p>反对人数：{oppNum}人</p>
-//     </div> 
-//   }
+  static contextType = ThemeContext
+  // this.props
+  render () {
+    const { store } = this.context
+    // 获取公共状态信息做绑定
+    let {supNum, oppNum} = store.getState()
+    return <div className="main">
+      <p>支持人数：{supNum}人</p>
+      <p>反对人数：{oppNum}人</p>
+    </div> 
+  }
 
-//   // 第一次渲染完毕，把让组件更新的办法，基于store.subscribe放到事件池中！！
-//   // 类组件，让组件更新，有更简单的操作：
-//   // 直接基于forceUpdate强制更新即可！！
-//   componentDidMount() {
-//     const {store} = this.context
-//     store.subscribe(() => {
-//       this.forceUpdate()
-//     })
-//   }
+  // 第一次渲染完毕，把让组件更新的办法，基于store.subscribe放到事件池中！！
+  // 类组件，让组件更新，有更简单的操作：
+  // 直接基于forceUpdate强制更新即可！！
+  componentDidMount() {
+    const {store} = this.context
+    store.subscribe(() => {
+      this.forceUpdate()
+    })
+  }
   
-// }
+}
 
 
-// export default VoteMain
+export default VoteMain
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,5 @@
 import { createStore } from 'redux'
-import { combineReducers } from 'redux'
-import voteReducer from './reducers/voteReducer'
-import personalReducer from './reducers/personalReducer'
-
-const reducer = combineReducers({
-  vote: voteReducer,
-  personal: personalReducer
-})
+import reducer from './reducers'
 
 // 创建store公共容器
 const store = createStore(reducer)
